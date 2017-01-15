a56893312a18a9b4d6efbc1ccd688bf9e5bb75cc
Seiji Emery <seijiemery@gmail.com>
2017-01-14 20:21:33 -0800

    Finished generator
    Prototype 1 complete

--- quiz_generator.js
+++ quiz_generator.js
@@ -2,27 +2,33 @@
 // and gen() (produces html math string), used by display.html. 
 var quiz_model=[
     {
-        section: "simple addition"
+        section: "Math: Simple Addition (Kindergarten)"
     },{
-        text: "a + 1, sum < 20",
-        gen: add_1(1000,2000)
+        text: "a + 0, 1 digit",
+        gen:  add_n(0,10, 0)
     },{
-        text: "a + 1, sum < 100",
-        gen: add_1(0,100)
+        text: "a + 1, 1 digit",
+        gen:  add_n(0,10, 1)
     },{
-        text: "1 digit, a + b, sum < 10",
-        gen: add_ab(1,1, 0,10)
+        text: "a + 2, 1 digit",
+        gen:  add_n(0,10, 2)
     },{
-        text: "1 digit, a + b, sum >= 10",
-        gen: add_ab(1,1, 10,20)
+        text: "a + 3, 1 digit",
+        gen:  add_n(0,10, 3)
     },{
-        text: "a + 0 and 0 + a, sum < 20",
-        gen: add_0(0,20)
+        text: "a + b >= 4, 1 digit",
+        gen:  add_ab(0,10, 4,10)
     },{
-        text: "a + 0 and 0 + a, sum < 100",
-        gen: add_0(0,100)
+        text: "1-3 + 0-20",
+        gen:  add_ab(1,3, 0,20)
     },{
-        section: "2-digit addition"
+        text: "1-3 + 20-50",
+        gen:  add_ab(1,3, 20,50)
+    },{
+        text: "1-3 + 50-100",
+        gen:  add_ab(1,3, 50,100)
+    },{
+        section: "Math: Addition (1st)"
     },{
         text: "2 digit a + 1 digit b",
         gen: add_ab(2,1)
@@ -45,60 +51,94 @@ var quiz_model=[
         gen: add_ab(3,3)
     },{
         section: "4-digit addition"
+    },{
+        text: "3 digit a + 2 digit b, result between [100, 250]",
+        gen: constrain_result(100,251, add_ab(0,1000, 0,100))
     }
 ];
 
-function add_1 (min_sum, max_sum) {
+// Add constraint
+function constrain_result (x_min, x_max, fcn, limit) {
+    limit = limit || 50;   // retry at most N iterations
     return function () {
-        var n = (Math.random() * (max_sum - min_sum) + min_sum)|0;
-        return gen_add_scramble(1, n);
-    }
-}
-function add_0 (min_sum, max_sum) {
-    return function () {
-        var n = (Math.random() * (max_sum - min_sum) + min_sum)|0;
-        return gen_add_scramble(0, n);
+        for (var i = limit; i --> 0; ) {
+            var x = fcn();
+            if (x.result < x_min || x.result >= x_max) continue; // retry
+            else break;
+        }
+        return x;
     }
 }
 
-// Addition problem:
-//  min/max a, b: non-optional numeric limits for the 2 arguments
-//  min/max sum:  optional numeric constraint on sum of 2 numbers
+// Addition a + n = ?, where
+//  a: random number bounded by a_min, a_max
+//  n: constant number (0, 1, 2, etc).
 //
-function add_ab (a_min, a_max, b_min, b_max, sum_min, sum_max) {
+function add_n (a_min, a_max, n) {
     return function () {
-        var a, b;
-        do {
-            a = (Math.random() * (a_max - a_min) + a_min)|0;
-            b = (Math.random() * (b_max - b_min) + b_min)|0;
-        } while (
-            (sum_min !== undefined && (a + b) < sum_min) ||
-            (sum_max !== undefined && (a + b) >= sum_max)
-        );
-        return gen_add_scramble(a, b);
+        var a = (Math.random() * (a_max - a_min) + a_min)|0;
+        return new AdditionProblem(a, n);
     }
 }
 
-// Randomly selects either (a, b) or (b, a) and passes arguments
-// to gen_add. Result is gen_add_scramble(0, 1) will produce
-// either '1 + 0' or '0 + 1'.
+// Addition a + b = ?, where
+//  a: random number bounded by a_min, a_max
+//  b: random number bounded by b_min, b_max
 //
-function gen_add_scramble (a, b) {
-    return Math.random() > 0.5 ?
-        gen_add(a, b) :
-        gen_add(b, a);
+function add_ab (a_min, a_max, b_min, b_max) {
+    return function () {
+        var a = (Math.random() * (a_max - a_min) + a_min)|0;
+        var b = (Math.random() * (b_max - b_min) + b_min)|0;
+        return new AdditionProblem(a, b);
+    }
 }
 
-// Generates math html for an addition problem
-function gen_add (a, b) {
-    return "<mn>"+a+"</mn> <mo>+</mo> <mn><mn>"+b+"</mn> <mo>=</mo>";
+function ArithmeticProblem (op, a, b) {
+    this.op = op;
+    if (!this.isCommutative || Math.random() >= 0.5)
+        this.a = a, this.b = b;
+    else
+        this.a = b, this.b = a;
+    this.result = eval(''+this.a+op+this.b);
 }
+ArithmeticProblem.prototype.equals = function (other) {
+    return this.op == other.op && this.a == other.a && this.b == other.b;
+}
+ArithmeticProblem.prototype.genMathML = function () {
+    return "<mn>"+this.a+"</mn> <mo>"+this.op+"</mo> <mn>"+this.b+"</mn> <mo>=</mo> ";
+}
+
+Function.prototype.subclass = function (parent, properties) {
+    this.prototype = Object.create(parent.prototype);
+    this.prototype.constructor = this;
+    for (var k in properties)
+        this.prototype[k] = properties[k];
+    return this;
+}
+
+
+function AdditionProblem (a, b) {
+    ArithmeticProblem.apply(this, ['+', a, b]);
+}
+AdditionProblem.subclass(ArithmeticProblem, {
+    isCommutative: true
+});
+
+(function SubtractionProblem (a, b) {
+    ArithmeticProblem.apply(this, ['-', a, b]);
+}).subclass(ArithmeticProblem, { isCommutative: false });
+
+(function MultiplicationProblem (a, b) {
+    ArithmeticProblem.apply(this, ['*', a, b]);
+}).subclass(ArithmeticProblem, { isCommutative: true });
+
+(function DivisionProblem (a, b) {
+    ArithmeticProblem.apply(this, ['/', a, b]);
+}).subclass(ArithmeticProblem, { isCommutative: false });
 
 
 // Display functions
 function display_generate (div, input) {
-
-    // window.alert("baz");
 
     // Generate selection of items from input.levels (must be Array + match quiz_model!)
     var items = [];
@@ -110,6 +150,21 @@ function display_generate (div, input) {
 
         // Generate n items for this level (or none if zero)
         for (var count = input.levels[i]; count --> 0; ) {
+
+            // Generate, and check that each item is unique
+            var maxAttempts = 100;
+            do {
+                var item = quiz_model[i].gen();
+
+                var isUnique = true;
+                for (var j = items.length; j --> 0; ) {
+                    if (item.equals(items[j])) {
+                        isUnique = false;
+                        break;
+                    }
+                }
+            } while (!isUnique && maxAttempts --> 0);
+
             items.push(quiz_model[i].gen());
         }
     }
@@ -140,7 +195,7 @@ function display_generate (div, input) {
     // Generate HTML + add to page
     items.forEach(function(item){
         var elem = document.createElement("p");
-        elem.innerHTML = item; 
+        elem.innerHTML = item.genMathML();
         div.appendChild(elem);
     });
 }
